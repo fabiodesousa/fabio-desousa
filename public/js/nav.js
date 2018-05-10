cec24dc253cec8a73b4616f5db7ca4bd38a774f5
Fabio DeSousa <mail@fabiodesousa.com>
2018-05-10 18:49:31 -0400

    init

---
+++ public/js/nav.js
@@ -0,0 +1,23 @@
+document.getElementById("navMenu").innerHTML = 
+'<div class = "navbar-header">'+
+  '<button type = "button" class = "navbar-toggle collapsed" '+
+    ' data-toggle = "collapse" data-target = ".navbar-collapse">'+
+    ' <span class = "sr-only">Toggle navigation</span>'+
+    ' <span class = "icon-bar"></span>'+
+    ' <span class = "icon-bar"></span>'+
+    ' <span class = "icon-bar"></span>'+
+  '</button>'+
+  '<a class = "navbar-brand" href = "http://fabiodesousa.com/index.html">Home<span class="sr-only"(current)</span></a>'+
+'</div>'+
+'<div class = "collapse navbar-collapse">'+
+/*  '<ul class="nav navbar-nav pull-left">'+
+    '<li class="nav-item"><a class="nav-link" href="http://fabiodesousa.com/index.html#work">Work</a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="http://fabiodesousa.com/resume.pdf" target="_blank">Resume</a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="mailto:mail@fabiodesousa.com">Contact</a></li>'+
+  '</ul>'+*/
+  '<ul class="nav navbar-nav pull-right">'+
+    '<li class="nav-item"><a class="nav-link" href="https://twitter.com/defabio_desousa" target="_blank"><span class="fa fa-twitter"></span></a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="https://www.instagram.com/defabio_desousa/" target="_blank"><span class="fa fa-instagram"></span></a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="https://github.com/fabiodesousa" target="_blank"><span class="fa fa-github"></span></a></li>'+
+  '</ul>'+
+'</div>';
